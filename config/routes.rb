1dc735f12e5654300b6ee45fc2a646170d5f5d20
Mateusz Palyz <mateusz.palyz@netguru.pl>
2021-04-11 22:13:43 +0200

    add routes for csrf example

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#root'
+
+  get 'csrf', to: 'pages#csrf'
+  get 'csrf_send_mail', to: 'pages#csrf_send_mail'
+  delete 'csrf_on/:id(.:format)', to: 'pages#destroy_csrf_on', as: 'csrf_on_destroy'
+  delete 'csrf_off/:id(.:format)', to: 'pages#destroy_csrf_off', as: 'csrf_off_destroy'
 end
